572904e3dac764adb3d3b0772c235f2359bbb3dc
sjohnston82 <sjohnston82@users.noreply.github.com>
2023-10-21 08:15:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const ratingContainer = document.getElementsByClassName("number-container");
+const ratingsCircles = document.getElementsByClassName("rating-circle");
+const submitBtn = document.getElementById("btn");
+
+for (let i = 0; i < ratingsCircles.length; i++) {
+  ratingsCircles[i].addEventListener("click", function () {
+    const current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  const activeRating = document.getElementsByClassName("active")[0];
+  if (activeRating) {
+    const url = "http://127.0.0.1:5500/success.html";
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("rating", activeRating.id);
+    window.location.href = url + "?" + searchParams.toString();
+  }
+});
